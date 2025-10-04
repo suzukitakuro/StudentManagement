32b40742e61b34d978e2274896636294724c4a9c
suzuki <suzukitakuro@icloud.com>
2025-10-05 00:17:12 +0900

    初回コミット

--- src/main/java/raisetech/student/management/Application.java
+++ src/main/java/raisetech/student/management/Application.java
@@ -3,19 +3,35 @@ package raisetech.student.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-  public static void main(String[] args) {
+    private String name = "suzuki";
+    private String age = "29";
 
-    SpringApplication.run(Application.class, args);
-  }
+    public static void main(String[] args) {
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello world";
-  }
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/studentinfo")
+    public String getstudentinfo() {
+        return name+""+age+"歳";
+    }
+
+
+    @PostMapping("/studentinfo")
+    public void setName(String name,String age) {
+        this.name = name;
+        this.age = age;
+
+    }
+    @PostMapping("/studentName")
+    public  void UpdatestudentName(String name){
+        this.name = name;
+    }
 }
